ee739f778662c92f551c1694b31d0b9ccbda166b
Puntakan <puntakanc@gmail.com>
2025-04-05 19:10:36 +0700

    Update code

--- lib/main.dart
+++ lib/main.dart
@@ -7,12 +7,13 @@ import 'package:edugo/pages/splash_screen.dart';
 import 'package:edugo/features/subject/screens/subject_manage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   await FirebaseApi().initNotification();
-
+  await dotenv.load();
   runApp(SplashScreenApp());
 }
 
